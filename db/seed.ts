19bfdebf7b4986fbb72297cabf150fe2d83c11e5
Duth Ng <rumduth1@gmail.com>
2025-06-24 11:35:49 -0500

    add user data and token auth.js

--- db/seed.ts
+++ db/seed.ts
@@ -4,8 +4,16 @@ const prisma = new PrismaClient();
 
 async function main() {
   await prisma.product.deleteMany();
+  await prisma.user.deleteMany();
+  await prisma.account.deleteMany();
+  await prisma.session.deleteMany();
+  await prisma.verificationToken.deleteMany();
+
   await prisma.product.createMany({
     data: sampleData.products,
+  });
+  await prisma.user.createMany({
+    data: sampleData.users,
   });
   console.log("Database seeded succefully");
 }
